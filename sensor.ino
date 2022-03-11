4431557019d1d378299b9097c22537d80c558edc
Jeffery Bennett <meandmybadself@gmail.com>
2022-03-11 08:39:22 -0600

    Updates sensor.ino to add time bracketing

--- sensor.ino
+++ sensor.ino
@@ -1,15 +1,31 @@
+// To count loops.
 int frame = 0;
+
+// To use every nth frame.
 int sampleRate = 20;
 
+// Bend variables
+// This is what no bend equals.
 int prevValue = 2210;
-int threshold = 375;
-
+// Only register a slap if the bend has increased by this much since the last check.
+int bendThreshold = 250;
+// We don't want slaps going off too closely together.
+int prevFrame = 0;
+// Minimum number of frames that had to have passed since the last slap.
+int frameThreshold = 500;
 void loop() {
     if ((frame++ % sampleRate) == 0) {
+        // Read value from bend sensor.
         int value = analogRead(A0);
-        int diff = value - prevValue;
-        if (diff > threshold) {
-            Spark.publish("slap");
+        
+        // The difference in bend between now & the previous check.
+        int bendDiff = value - prevValue;
+        
+        // The difference in frames 
+        int frameDiff = frame - prevFrame;
+        if (bendDiff > bendThreshold && frameDiff > frameThreshold) {
+            Spark.publish("slap", String(frame) + "-" + String(prevFrame) + "=" + String(frameDiff));
+            prevFrame = frame;
         }
         prevValue = value;
     }
